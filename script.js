a9bc1a85e51b187f8b08ff8138d113c3e3876c1d
MOSTAFA <rawanahmed681@gmail.com>
2024-09-22 02:08:48 +0300

    create functions for adding notes

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
-function addNote(){
+var titleInput= document.getElementById('title');
+var descInput= document.getElementById('description');
+
+var notesContainer = [];
+
+
+function displayModal(){
     modal.style.display = "block";
 }
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+function closeModal(){
+    modal.style.display = "none";
+}
+
+function addNote(){
+    var note = {
+        title: titleInput.value,
+        description: descInput.value
+    }
+    notesContainer.push(note);
+
+    // console.log(note.title)
+    displayNote(notesContainer);
+}
+function displayNote(notesContainer){
+    var allNotes=``;
+    for( var i=0; i<notesContainer.length;i++){
+        allNotes +=`
+                <div class="note-content">
+                    <div class="note-head">
+                        <h1>${notesContainer[i].title}</h1>
+                        <i class="fa-solid fa-ellipsis"></i>
+                    </div>
+                    <p>${notesContainer[i].description}</p>
+                    <div class="note-type">
+                        <input type="checkbox" name="" id="done">
+                        <label for="done">Done</label>
+                    </div>
+                </div>
+            
+        `
+        document.getElementById('notecontent').innerHTML= allNotes;
+    }
+}
+console.log(notesContainer)
